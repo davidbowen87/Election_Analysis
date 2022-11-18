8de0524f8df3717f7f8adffd8f09972e392dee0d
git@github.com:davidbowen87/Better.git <david.bowen@Hajoca.com>
2022-11-18 13:40:31 -0500

    Updating code in PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -1,3 +1,22 @@
-with open("UR-VIRT-DATA-PT-11-2022-U-B/03-Python/03-Homework/Resources/election_results.csv") as election_data:
+import csv
+
+import os
+
+#Assign a variable to load a file from a path
+
+file_to_load = os.path.join("election_results.csv")
+
+#Assign a variable to save the file to a path. 
+
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+#Open the election results and read the file.
+
+with open(file_to_load) as election_data:
+
+    file_reader = csv.reader(election_data)
+
+    headers = next(file_reader)
+
+    print(headers)
     
-    print(election_data)
