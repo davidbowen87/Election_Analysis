6a401daf04dcd949b2e3661cee369d8ef99d9b13
David Bowen <davidbowen1776@gmail.com>
2022-11-19 13:51:22 -0500

    restructure fies

---
+++ write_election_results.py
@@ -0,0 +1,8 @@
+import os
+
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+with open(file_to_save, "w") as txt_file:
+
+    txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
+
